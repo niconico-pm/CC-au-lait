ecc5df1a5778adc7e373a7fde5ad170e2a39bc32
YukitoYamaguchi <touhouyukito@yahoo.co.jp>
2014-12-10 01:51:08 +0900

    modified:   topran.py

--- topran.py
+++ topran.py
@@ -8,25 +8,34 @@ class MyHTMLParser(HTMLParser):
         self.nowtag = ''
         self.nowattr = ''
         self.scorelist = []
-        self.score = [None] * 4
-        self.scorearea_depth = 0
+        self.score = [None] * 7
+        self.difarea_depth = 0
         self.inscorearea = False
         self.dif = 0
         HTMLParser.__init__(self)
-        
-    def handle_data(self, data):
-        if self.nowtag == 'div' or self.nowtag == 'span':
-            for name, value in self.nowattr:
-                if name  == 'class':
-                    if value == 'mymusic_tit':
-                        self.score[0] = data 
-                    if self.dif == 'light':
-                        self.score[1] = data
-                    if self.dif == 'medium':
-                        self.score[2] = data
-                    if self.dif == 'beast':
-                        self.score[3] = data                                        
 
+    def handle_data(self, data):
+        if self.nowtag == 'div' or self.nowtag == 'span' or self.nowtag == 'img':
+            for name, value in self.nowattr:
+                if name == 'class':
+                    if value == 'mymusic_tit':
+                        self.score[0] = data
+                    else:
+                        for i in range(1,4):
+                            if self.dif == i:
+                                if value == 'score_num':
+                                    self.score[(2 * i) - 1] = data
+                                if value == 'perfect':
+                                    self.score[2 * i] = 'Perfect'
+                                if value == 'fullcombo':
+                                    self.score[2 * i] = 'FullCombo'
+                                if value == 'clear':
+                                    self.score[2 * i] = 'Clear'
+                                if value == 'failed':
+                                    self.score[2 * i] = 'Failed'
+                                if value == 'grade':
+                                    self.dif = 0
+    
     def handle_starttag(self, tag, attr):
         self.nowtag = tag
         self.nowattr = attr
@@ -35,20 +44,19 @@ class MyHTMLParser(HTMLParser):
                 self.scorearea_depth += 1
                 for name, value in attr:
                     if value == 'light':
-                        self.dif = value
+                        self.dif = 1
                     if value == 'medium':
-                        self.dif = value
+                        self.dif = 2
                     if value == 'beast':
-                        self.dif = value
+                        self.dif = 3
+
             else:
                 for name, value in attr:
-                    if name  == 'class' and value == 'score_area':
+                    if name == 'class' and value == 'score_area':
                         self.scorearea_depth = 1
-                        self.dif = 1
                         self.inscorearea = True
-                        self.score[1] = None                    
-                        self.score[2] = None                    
-                        self.score[3] = None
+                        for i in range(1,7):
+                            self.score[i] = None
 
     def handle_endtag(self, tag):
         if tag == 'div':
@@ -72,9 +80,9 @@ parser = MyHTMLParser()
 parser.feed(allLines)
 
 scorelist = parser.get_scorelist()
-print scorelist
+#print scorelist
 for score in scorelist:
     print "曲名   :", score[0]
-    print "Light  :", score[1]
-    print "Medium :", score[2]
-    print "Beast  :", score[3]
+    print "Light  :", score[1], score[2]
+    print "Medium :", score[3], score[4]
+    print "Beast  :", score[5], score[6]
