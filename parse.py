935f1c96abd8f4a9b16d0e89f27eefae179e5c84
kentahama <touhouyukito@yahoo.co.jp>
2014-12-10 16:36:16 +0900

    タプル二重化 	modified:   parse.py

--- parse.py
+++ parse.py
@@ -8,8 +8,9 @@ class MyHTMLParser(HTMLParser):
         self.nowtag = ''
         self.nowattr = ''
         self.scorelist = []
-        self.score = [None] * 7
-        self.difarea_depth = 0
+        self.title = ''
+        self.score = [None] * 3
+        self.medal = [None] * 3
         self.inscorearea = False
         self.dif = 0
         HTMLParser.__init__(self)
@@ -19,22 +20,18 @@ class MyHTMLParser(HTMLParser):
             for name, value in self.nowattr:
                 if name == 'class':
                     if value == 'mymusic_tit':
-                        self.score[0] = data
+                        self.title += data
                     else:
-                        for i in range(1,4):
-                            if self.dif == i:
-                                if value == 'score_num':
-                                    self.score[(2 * i) - 1] = data
-                                if value == 'perfect':
-                                    self.score[2 * i] = 'Perfect'
-                                if value == 'fullcombo':
-                                    self.score[2 * i] = 'FullCombo'
-                                if value == 'clear':
-                                    self.score[2 * i] = 'Clear'
-                                if value == 'failed':
-                                    self.score[2 * i] = 'Failed'
-                                if value == 'grade':
-                                    self.dif = 0
+                        if value == 'score_num':
+                            self.score[self.dif] = data
+                        if value == 'perfect':
+                            self.medal[self.dif] = 3
+                        if value == 'fullcombo':
+                            self.medal[self.dif] = 2
+                        if value == 'clear':
+                            self.medal[self.dif] = 1
+                        if value == 'failed':
+                            self.medal[self.dif] = 0
     
     def handle_starttag(self, tag, attr):
         self.nowtag = tag
@@ -44,27 +41,28 @@ class MyHTMLParser(HTMLParser):
                 self.scorearea_depth += 1
                 for name, value in attr:
                     if value == 'light':
-                        self.dif = 1
+                        self.dif = 0
                     if value == 'medium':
-                        self.dif = 2
+                        self.dif = 1
                     if value == 'beast':
-                        self.dif = 3
+                        self.dif = 2
 
             else:
                 for name, value in attr:
                     if name == 'class' and value == 'score_area':
                         self.scorearea_depth = 1
                         self.inscorearea = True
-                        for i in range(1,7):
-                            self.score[i] = None
+                        self.score = [None] * 3
+                        self.medal = [None] * 3
 
     def handle_endtag(self, tag):
         if tag == 'div':
             if self.inscorearea:
                 self.scorearea_depth -= 1
                 if self.scorearea_depth == 0:
-                    self.scorelist.append(tuple(self.score))
+                    self.scorelist.append((self.title,tuple(self.score),tuple(self.medal)))
                     self.inscorearea = False
+                    self.title = ''
         if tag == self.nowtag:
             self.nowtag = ''
             self.nowattr = ''
@@ -80,7 +78,7 @@ parser = MyHTMLParser()
 parser.feed(allLines)
 
 scorelist = parser.get_scorelist()
-#print scorelist
+print scorelist
 song_num = 0
 l_num = 0
 m_num = 0
@@ -105,48 +103,48 @@ mn = 0
 bn = 0
 for score in scorelist:
     print "曲名   :", score[0]
-    print "Light  :", score[1], score[2]
-    print "Medium :", score[3], score[4]
-    print "Beast  :", score[5], score[6]
-    if score[1] != None:
-        l_total += int(score[1])
+    print "Light  :", score[1][0], score[2][0]
+    print "Medium :", score[1][1], score[2][1]
+    print "Beast  :", score[1][2], score[2][2]
+    if score[1][0] != None:
+        l_total += int(score[1][0])
         l_num += 1
-    if score[3] != None:
-        m_total += int(score[3])
+    if score[1][1] != None:
+        m_total += int(score[1][1])
         m_num += 1
-    if score[5] != None:
-        b_total += int(score[5])
+    if score[1][2] != None:
+        b_total += int(score[1][2])
         b_num += 1
-    if score[2] != None:
-        if score[2] == 'Perfect':
+    if score[2][0] != None:
+        if score[2][0] == 3:
             lp += 1
-        if score[2] == 'FullCombo':
+        if score[2][0] == 2:
             lf += 1
-        if score[2] == 'Clear':
+        if score[2][0] == 1:
             lc += 1
-        if score[2] == 'Failed':
+        if score[2][0] == 0:
             lb += 1
     else:
         ln += 1
-    if score[4] != None:
-        if score[4] == 'Perfect':
+    if score[2][1] != None:
+        if score[2][1] == 3:
             mp += 1
-        if score[4] == 'FullCombo':
+        if score[2][1] == 2:
             mf += 1
-        if score[4] == 'Clear':
+        if score[2][1] == 1:
             mc += 1
-        if score[4] == 'Failed':
+        if score[2][1] == 0:
             mb += 1
     else:
         mn += 1
-    if score[6] != None:
-        if score[6] == 'Perfect':
+    if score[2][2] != None:
+        if score[2][2] == 3:
             bp += 1
-        if score[6] == 'FullCombo':
+        if score[2][2] == 2:
             bf += 1
-        if score[6] == 'Clear':
+        if score[2][2] == 1:
             bc += 1
-        if score[6] == 'Failed':
+        if score[2][2] == 0:
             bb += 1
     else:
         bn += 1
